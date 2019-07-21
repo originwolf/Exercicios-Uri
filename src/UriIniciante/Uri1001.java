359e09b2cf604f9d96457c7bd2a02e5379dbf6c9
Pedro Martins <pedrogmhead@gmail.com>
2019-07-21 18:53:09 -0300

    Added exercise 1001
    already done

---
+++ src/UriIniciante/Uri1001.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UriIniciante;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pedri
+ */
+public class Uri1001 {
+    
+    public static void main(String[] args) {
+        
+        int a, b, x;
+        
+        Scanner ler = new Scanner(System.in);
+        
+        a = ler.nextInt();
+        b = ler.nextInt();
+        
+        x = a + b;
+        
+        System.out.println("X = "+x);
+        
+    }
+    
+}
